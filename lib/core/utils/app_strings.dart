c845e21f527b162ddb7d5e30d5bf871bccbb776e
='ehabgalal <=ehabgalal1312@gmail.com>
2025-10-01 19:04:34 +0300

    add app string

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,12 +1,79 @@
-import 'package:flutter/material.dart';
-
 abstract class AppStrings {
-  static Color primaryColor = const Color(0xffC49E85);
-  static Color offWhite = const Color(0xffF8F4F9);
-  static Color darkBrown = const Color(0xff6B4B3E);
-  static Color darkGrey = const Color(0xff6F6460);
-  static Color lightGrey = const Color(0xffB4B4B4);
-  static Color dark = const Color(0xff6F6460);
-  static Color black = const Color(0xff333333);
+  //ToDo:All Strings
+
+  static const String appName = "Dalel",
+      //!On Boarding Strings
+      skip = "Skip",
+      next = "Next",
+      createAccount = "Create Account",
+      loginNow = "Login Now",
+      //!Auth Strings
+      welcome = "Welcome!",
+      welcomeBack = "Welcome Back!",
+      fristName = "First Name",
+      lastName = "Last Name",
+      emailAddress = "Email Address",
+      password = "Password",
+      iHaveAgreeToOur = "I have agree to our ",
+      termsAndCondition = "Terms and Condition",
+      signUp = "Sign Up",
+      signIn = "Sign In",
+      alreadyHaveAnAccount = "Already have an account ? ",
+      dontHaveAnAccount = "Don’t have an account ? ",
+      forgotPassword = "Forgot Password ?",
+      //!Home Strings
+      recommendations = "Recommendations",
+      about = "About",
+      wars = "Wars",
+      //!Search Strings
+      searchDeepIntoTheHistory = "Search deep into the history",
+      recent = "Recent",
+      //!Profile Strings
+      profile = "Profile",
+      account = "Account",
+      editProfile = "Edit Profile",
+      notification = "Notification",
+      general = "General",
+      settings = "Settings",
+      security = "Security",
+      privacyPolicy = "Privacy Policy",
+      logOut = "Log Out",
+      //!Bazar
+      bazar = "Bazar",
+      historicalPeriods = "Historical periods",
+      historicalCharacters = "Historical Characters",
+      historyBooks = "History Books",
+      historicalSouvenirs = "Historical Souvenirs",
+      //!Cart
+      myCart = "My Cart",
+      totalPayment = "Total Payment",
+      checkoutNow = "Checkout Now",
+      //!Payment Strings
+      checkoutScreen = "Checkout Screen",
+      deliveryAddress = "Delivery Address",
+      change = "Change",
+      items = "1 items",
+      selectedProduct = "Selected Product",
+      paymentMethod = "Payment Method",
+      confirmPayment = "Confirm Payment",
+      orderSuccess = "Order Success",
+      thankYou =
+          "Thank you for your order here and your package will be sent to your address very quickly and fast good product",
+      backToHome = "Back To Home",
+      //!Forgot password
+      forgotPasswordPage = "Forgot Password",
+      sendResetPasswordLink = "Send Reset Passwrod Link",
+      verificationNow = "Verification Now",
+      resendCode = "Resend Code",
+      verifyAccount = "Verify Account",
+      enter4DigitCodeWeHaveSentTo = "Enter 4 digit code we have sent to ",
+      haventReceivedVerificationCode = "Haven’t received verification code?";
 }
- 
+
+abstract class FireBaseStrings {
+  static const String historicalPeriods = "historical_periods";
+  static const String name = "name";
+  static const String description = "description";
+  static const String image = "image";
+  static const String wars = "wars";
+}
