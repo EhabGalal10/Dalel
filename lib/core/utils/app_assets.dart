dd6f835fa405420fd1c0f8975c55e94034eaa1fe
='ehabgalal <=ehabgalal1312@gmail.com>
2025-10-01 14:30:39 +0300

    prepare photos

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesFrame1
+  /// assets/images/Frame1.png
+  static const String imagesFrame1 = "assets/images/Frame1.png";
+
+  /// Assets for imagesOnbording1
+  /// assets/images/onbording1.png
+  static const String imagesOnbording1 = "assets/images/onbording1.png";
+
+  /// Assets for imagesOnbording2 
+  /// assets/images/onbording2 .png
+  static const String imagesOnbording2  = "assets/images/onbording2 .png";
+
+  /// Assets for imagesOnbording3 
+  /// assets/images/onbording3 .png
+  static const String imagesOnbording3  = "assets/images/onbording3 .png";
+
+  /// Assets for imagesSuccess
+  /// assets/images/Success.png
+  static const String imagesSuccess = "assets/images/Success.png";
+
+  /// Assets for imagesVector
+  /// assets/images/Vector.svg
+  static const String imagesVector = "assets/images/Vector.svg";
+
+  /// Assets for imagesVector1
+  /// assets/images/Vector1.svg
+  static const String imagesVector1 = "assets/images/Vector1.svg";
+}
+
---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStrings {
+  static Color primaryColor = const Color(0xffC49E85);
+  static Color offWhite = const Color(0xffF8F4F9);
+  static Color darkBrown = const Color(0xff6B4B3E);
+  static Color darkGrey = const Color(0xff6F6460);
+  static Color lightGrey = const Color(0xffB4B4B4);
+  static Color dark = const Color(0xff6F6460);
+  static Color black = const Color(0xff333333);
+}
+ 
